453fe9f56b981d34f05409a75b4504c0aea9aa76
lantelyes <antell@rpi.edu>
2016-02-21 20:23:28 -0500

    Integrate OSC into VS Project

---
+++ Shotgun_Prototype/Plugins/OSC/Intermediate/Build/Win64/UE4Editor/Development/OSC/OscPrivatePCH.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_OSCPRIVATEPCH_H__
+#define __AUTO_OSCPRIVATEPCH_H__
+//Last Write: 2/22/2016 1:19:29 AM +00:00
+#include "C:\Users\Leo\Desktop\ShotgunGladiators\Shotgun_Prototype\Plugins\OSC\Source\OSC\Private\OscPrivatePCH.h"
+#endif//__AUTO_OSCPRIVATEPCH_H__
---
+++ Shotgun_Prototype/Plugins/OSC/Intermediate/Build/Win64/UE4Editor/Development/OSC/PCH.OscPrivatePCH.h.cpp
@@ -0,0 +1,1 @@
+#include "C:\Users\Leo\Desktop\ShotgunGladiators\Shotgun_Prototype\Plugins\OSC\Source\OSC\Private\OscPrivatePCH.h"
