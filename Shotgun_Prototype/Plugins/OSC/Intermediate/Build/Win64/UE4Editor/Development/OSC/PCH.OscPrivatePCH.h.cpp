cf9eb692627c12567b5cf641491246e25533d77d
Chris <cloutc@rpi.edu>
2016-02-24 14:12:05 -0500

    OSC Stuff? IDK

--- Shotgun_Prototype/Plugins/OSC/Intermediate/Build/Win64/UE4Editor/Development/OSC/PCH.OscPrivatePCH.h.cpp
+++ Shotgun_Prototype/Plugins/OSC/Intermediate/Build/Win64/UE4Editor/Development/OSC/PCH.OscPrivatePCH.h.cpp
@@ -1,1 +1,1 @@
-#include "C:\Users\Leo\Desktop\ShotgunGladiators\Shotgun_Prototype\Plugins\OSC\Source\OSC\Private\OscPrivatePCH.h"
+#include "C:\Users\Chris C\Documents\ShotgunGladiators\Shotgun_Prototype\Plugins\OSC\Source\OSC\Private\OscPrivatePCH.h"
