24a1b67212c9abdc7324e78f696f107d13ecc2cc
Spencer <johnss18@rpi.edu>
2016-04-09 09:23:02 -0400

    added shield bashing, but it  1hit KOs so theres that

--- Shotgun_Prototype/Source/Shotgun_Prototype/Public/ShieldBashState.h
+++ Shotgun_Prototype/Source/Shotgun_Prototype/Public/ShieldBashState.h
@@ -16,6 +16,16 @@ class SHOTGUN_PROTOTYPE_API UShieldBashState : public UUpperBodyState
 public:
 	virtual void OnBeginState() override;
 	virtual void TickState(float DeltaTime) override;
+
+	bool bAlreadyHit;
 	
-	
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FShieldBashDelegate);
+	UPROPERTY(BlueprintAssignable, Category = "Gladiator/ShieldBash")
+	FShieldBashDelegate ShieldBashEvent;
+
+	UFUNCTION(Server, Reliable, WithValidation)
+	void DoShieldBash_Server();
+
+	UFUNCTION(Server, Reliable, WithValidation, BlueprintCallable, Category ="Gladiator/ShieldBash")
+	void SetAlreadyHit_Server();
 };
