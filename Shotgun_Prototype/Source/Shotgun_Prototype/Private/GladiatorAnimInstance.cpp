88ee17bc097fb6d18da64f7255e63dd5d20e70a8
DomCristaldi <domeniccristaldi@gmail.com>
2016-04-19 02:28:02 -0400

    ++ Created c++ class as the base of the Gladiator's Animation BP. Gonna move a bunch of mathematical functionality over to it ++ Idle Hip Rotation is in. Gotta look into why the legs jitter (already have a lead)

--- Shotgun_Prototype/Source/Shotgun_Prototype/Private/GladiatorAnimInstance.cpp
+++ Shotgun_Prototype/Source/Shotgun_Prototype/Private/GladiatorAnimInstance.cpp
@@ -8,7 +8,8 @@ void UGladiatorAnimInstance::NativeInitializeAnimation()
     Super::NativeInitializeAnimation();
 
     //OBTAIN THE OWNING PAWN
-    OwningPawn = TryGetPawnOwner();
+    owningPawn = TryGetPawnOwner();
+    world = GetWorld();
 }
 
 void UGladiatorAnimInstance::NativeUpdateAnimation(float DeltaTimeX)
@@ -16,6 +17,27 @@ void UGladiatorAnimInstance::NativeUpdateAnimation(float DeltaTimeX)
     Super::NativeUpdateAnimation(DeltaTimeX);
 
     //DO NOT ADVANCE IF WE DON'T HAVE AN OWNING PAWN
-    if (!OwningPawn) { return; }
+    if (!owningPawn) { return; }
 
+}
+
+
+void UGladiatorAnimInstance::FootTrace(USkeletalMeshComponent* mesh, FVector rootPos, FVector ikTraceStartPos, FVector ikTraceEndPos, FVector footAnkeOffset, ECollisionChannel traceChannel, TArray<AActor*> ignoredActors,
+                                       bool& hit, FVector& hitLocation, float& hipOffset, FRotator& footRotation, FVector& hitNormal)
+{
+
+    //FCollisionQueryParams(FName(TEXT("IK Foot Trace")), false, ignoredActors);
+
+    FCollisionQueryParams collisionParams;
+    collisionParams.AddIgnoredActors(ignoredActors);
+
+    FHitResult hitResult;
+
+    world->LineTraceSingleByChannel(hitResult, ikTraceStartPos, ikTraceEndPos, traceChannel, collisionParams);
+
+
+    hit = hitResult.bBlockingHit;
+
+
+    
 }
--- Shotgun_Prototype/Source/Shotgun_Prototype/Public/GladiatorAnimInstance.h
+++ Shotgun_Prototype/Source/Shotgun_Prototype/Public/GladiatorAnimInstance.h
@@ -13,12 +13,22 @@ class SHOTGUN_PROTOTYPE_API UGladiatorAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
 	
+//SETUP
 public:
 
-    APawn* OwningPawn;
+    APawn* owningPawn;
+    UWorld* world;
 
     virtual void NativeInitializeAnimation() override;
 	
     virtual void NativeUpdateAnimation(float DeltaTimeX) override;
+
+
+//IK FUNCTIONS
+public:
+
+    UFUNCTION(BlueprintCallable, BlueprintCosmetic, Category = "IK|FootTrace")
+    void FootTrace(USkeletalMeshComponent* mesh, FVector rootPos, FVector ikTraceStartPos, FVector ikTraceEndPos, FVector footAnkeOffset, ECollisionChannel traceChannel, TArray<AActor*> ignoredActors,
+                   bool& hit, FVector& hitLocation, float& hipOffset, FRotator& footRotation, FVector& hitNormal);
 	
 };
